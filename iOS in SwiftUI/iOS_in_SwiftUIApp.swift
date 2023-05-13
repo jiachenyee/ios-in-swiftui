209c7a1c66070df2a8c14fc2d6b4aa5a57886fdf
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2023-05-13 11:02:21 +0700

    Initial Commit

---
+++ iOS in SwiftUI/iOS_in_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_in_SwiftUIApp.swift
+//  iOS in SwiftUI
+//
+//  Created by Jia Chen Yee on 13/5/23.
+//
+
+import SwiftUI
+
+@main
+struct iOS_in_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
